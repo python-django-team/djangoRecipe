d72d74e074742500a9186d818cfc3ab41f22fd56
k18083kk <k18083kk@pck18083kk.local>
2021-02-25 04:02:27 +0900

    searchResult checkbox 完成

--- static/js/myrecipe.js
+++ static/js/myrecipe.js
@@ -1,3 +1,17 @@
-() => {
-  document.getElementsByClassName("recipe-result-header");
-};
+d1 = document.getElementById("recipe_result");
+d2 = document.getElementById("select-myrecipe");
+
+// デフォルト非表示
+d2.style.display = "none";
+
+function clickBtn1() {
+    // 選択に変える
+    d1.style.display = "none";
+    d2.style.display = "block";
+}
+
+function clickBtn2() {
+    // 戻す
+    d2.style.display = "none";
+    d1.style.display = "block";
+}
