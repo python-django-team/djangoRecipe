c50bf03bb8b778bcca2c3ced3b38c02cd2577144
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-03-04 18:01:55 +0900

    Modify settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ from pathlib import Path
 import os
 from django.contrib import messages
 import environ
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
