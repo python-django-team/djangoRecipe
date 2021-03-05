4fce28a8a9a463aaa1b3c08eee780a409a0f45eb
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-03-05 18:18:52 +0900

    Modify

---
+++ project/local_settings.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'), #修正
+    }
+}
+DEBUG = True
